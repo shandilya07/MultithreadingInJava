fa1e21f387b489a46e5fc345b808b8f7bd3cb141
Shandilya <135587831+shandilya07@users.noreply.github.com>
2024-04-05 14:34:02 +0530

    Update JoinThreadExample.java

--- src/main/java/basicMultithreading/JoinThreadExample.java
+++ src/main/java/basicMultithreading/JoinThreadExample.java
@@ -3,20 +3,21 @@ package basicMultithreading;
 public class JoinThreadExample {
     public static void main(String[] args) throws InterruptedException {
         Thread one = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.println("Thread 1 : " + i);
             }
         });
 
         Thread two = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 25; i++) {
                 System.out.println("Thread 2 : " + i);
             }
         });
-        System.out.println("Before invoking threads executing the threads!");
+
+        System.out.println("Before invoking threads...");
         one.start();
-        one.join();
         two.start();
+        one.join();
         System.out.println("Done executing the threads!");
     }
 }
