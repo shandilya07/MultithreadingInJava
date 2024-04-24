cb39ee0a0c4fe046408e0a05f741f71c9690b5d5
shandilya07 <shandilya.codes@gmail.com>
2024-04-25 00:58:35 +0530

    Cyclic Barrier

---
+++ src/main/java/concurrentCollection/MultiStageTour.java
@@ -0,0 +1,48 @@
+package concurrentCollection;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class MultiStageTour {
+
+    private static final int NUM_TOURISTS = 5;
+    private static final int NUM_STAGES = 3;
+    private static final CyclicBarrier barrier = new CyclicBarrier(NUM_TOURISTS,() -> {
+        System.out.println("Tour guide starts speaking...");
+    });
+
+    public static void main(String[] args) {
+        for (int i = 0; i < NUM_TOURISTS; i++) {
+            Thread touristThread = new Thread(new Tourist(i));
+            touristThread.start();
+        }
+    }
+
+    static class Tourist implements Runnable {
+        private final int touristId;
+
+        public Tourist(int touristId) {
+            this.touristId = touristId;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < NUM_STAGES; i++) {
+                // Perform actions at each stage
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Tourist " + touristId + " arrives at Stage " + (i + 1));
+
+                // Wait for all tourists to arrive at the current stage
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
